8d1654e269931af3c8df94f6c5ebeeebf59d28e6
LockedX <1130876885@qq.com>
2019-03-21 18:11:34 +0800

    添加工厂模式类图

---
+++ a-design-pattern/src/test/java/com/lx/learning/test/Test.java
@@ -0,0 +1,12 @@
+package com.lx.learning.test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] a = new int[]{-4,-2,-1,5,8,11};
+        System.out.println(Arrays.stream(a).map( x -> x*x ).sorted().toArray()[0]);
+    }
+}
