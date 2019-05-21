64db77e64b8654b670e8fb31c4b4e208e8d88eec
LockedX <1130876885@qq.com>
2019-05-21 09:43:41 +0800

    CompletableFuture

--- c-concurrency/src/main/java/com/lx/learning/concurrency/CompletableFutureDemo2.java
+++ c-concurrency/src/main/java/com/lx/learning/concurrency/CompletableFutureDemo2.java
@@ -7,11 +7,12 @@ import java.util.concurrent.*;
  * @author lanxing
  */
 public class CompletableFutureDemo2 {
-    private static final Random random=new Random();
+    private static final Random random = new Random();
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService service = Executors.newFixedThreadPool(3);
-        int value=random.nextInt(200);
-        System.out.println("value==>"+value);
+        int value = random.nextInt(200);
+        System.out.println("value==>" + value);
         Future<String> future = service.submit(() -> {
             System.out.println("before");
             Thread.sleep(value);
@@ -19,13 +20,13 @@ public class CompletableFutureDemo2 {
             return "hello";
         });
         try {
-            System.out.println(future.get(100,TimeUnit.MILLISECONDS));
+            System.out.println(future.get(100, TimeUnit.MILLISECONDS));
         } catch (TimeoutException e) {
             System.out.println("Time Out");
             //true表示剩下的代码也会执行
             //false表示剩下的代码不执行了
             future.cancel(false);
-        }finally {
+        } finally {
             service.shutdown();
         }
 
