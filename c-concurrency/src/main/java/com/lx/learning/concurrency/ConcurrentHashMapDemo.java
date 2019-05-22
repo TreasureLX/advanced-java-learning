3df0fbe02df3789f75ebadb14ba5a1a596478992
LockedX <1130876885@qq.com>
2019-05-22 15:00:38 +0800

    CompletableFuture

---
+++ c-concurrency/src/main/java/com/lx/learning/concurrency/ConcurrentHashMapDemo.java
@@ -0,0 +1,10 @@
+package com.lx.learning.concurrency;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashMapDemo {
+    public static void main(String[] args) {
+        ConcurrentHashMap chm=new ConcurrentHashMap();
+        chm.put("1","eee");
+    }
+}
