c5f473809353861eafb75268be4abc52f07f1c64
LockedX <1130876885@qq.com>
2019-06-14 18:24:44 +0800

    类加载机制

--- b-collections/src/main/java/com/lanxing/collections/MapDemo.java
+++ b-collections/src/main/java/com/lanxing/collections/MapDemo.java
@@ -1,9 +1,6 @@
 package com.lanxing.collections;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.TreeSet;
+import java.util.*;
 
 public class MapDemo {
     public static void main(String[] args) {
@@ -12,6 +9,22 @@ public class MapDemo {
         map.put("b","eeee");
         HashSet<String> strings=new HashSet<>();
         TreeSet<String> treeSet=new TreeSet<>();
+
+        treeSet.add("a");
+        treeSet.add("b");
+        treeSet.add("c");
+        treeSet.add("d");
+
+        System.out.println(treeSet.tailSet("b"));
+
+        TreeMap treeMap=new TreeMap();
+        treeMap.put("1","hhh");
+        treeMap.put("2","rrrr");
+        treeMap.put("3","rttyy");
+        treeMap.put("4","dfgrt");
+        treeMap.put("9","rere");
+
+        System.out.println(treeMap.tailMap("5"));
 
     }
 }
